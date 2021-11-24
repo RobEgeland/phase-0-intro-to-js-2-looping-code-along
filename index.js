ed49a96447ec878d30a159ee15edef04b32d9166
Robert Egeland <robegeland@yahoo.com>
2021-11-23 19:06:36 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+
+let thankYouMessages = [];
+
+function writeCards(thankYouMessages, event) {
+    for (let i = 0; i < thankYouMessages.length; i++) {
+        thankYouMessages.splice(i, 1, `Thank you, ${thankYouMessages[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+    
+    return thankYouMessages 
+}
+
+writeCards(["Lisa", "Kaitlin", "Jan"], "Birthday");
+
+function countDown(x) {
+    while (x >= 0) {
+        console.log(x);
+        x--
+    }
+
+} 
+
+
+
+
+ 
